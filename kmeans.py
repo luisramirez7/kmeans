6e6e3810f33fdfe730ee2f2ecb7211e4bb16e8c4
luisramirez7 <luisramirez2@huskers.unl.edu>
2019-02-24 15:54:30 -0600

    re-randomized cluster centers using sample, added main method

--- kmeans.py
+++ kmeans.py
@@ -1,13 +1,21 @@
 from parsing import get_records
-# Returns 
+import sys
+import random
+
+# Take distance measure of each transaction  
+def take_distance():
+    return
+
 def k_means(k, dataset):
-    records = dataset.records 
-    centers = records[:k]
+    records = dataset
+    random_centers = random.sample(records, k)
+
     changed = False
-    while changed: 
+    while changed:
         pass
 
-def main():
-    get_records('iris.arff')
-
-main()
+if __name__ == "__main__":
+    data_location = 'iris.arff'
+    data = get_records(data_location)
+    k = int(sys.argv[1])
+    k_means(k, data)
