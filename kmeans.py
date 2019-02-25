be9d670a4c76727193ad69b57415aaf2fd55e7fe
luisramirez7 <luisramirez2@huskers.unl.edu>
2019-02-24 19:14:16 -0600

    correctly returning a dataframe of euclidean distances between centroids

--- kmeans.py
+++ kmeans.py
@@ -5,20 +5,24 @@ import pandas as pd
 import numpy as np
 # Take distance measure of each transaction  
 def take_distance(centroid, record):
-    distance = record - centroid
+    distance = []
+    for vector in record:
+        euclidean_distance = np.linalg.norm(vector-centroid)
+        distance.append(euclidean_distance)
+    
     return distance 
 
 def k_means(k, dataset):
     random_centers = dataset.sample(n=k)
     
     centroids = list(random_centers.values)
-    
+    df = pd.DataFrame()
     # distance_frame = dataset.values - random_centers.values
     for index, centroid in enumerate(centroids):
-        print("Centroid #_{}".format(index), "Center: {}".format(centroid))
         values = take_distance(centroid, dataset.values)
-        distance_set = pd.DataFrame(values, columns=dataset.columns)
-        
+        df['Distance to Centroid_{}'.format(index)] = (values)
+    
+    
     # distances_to_centroids = pd.DataFrame()
     # distance = take_distance(random_centers[i], dataset)
 
